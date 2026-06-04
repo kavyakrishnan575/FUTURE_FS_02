b2346d4f88a984f01e5567754799f2c2a6f23b20
kavyakrishnan575 <kavyakrishnankathu@gmail.com>
2026-06-03 23:48:40 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
-console.log("Mini CRM");
+function addLead() {
+
+    let name = document.getElementById("name").value;
+    let email = document.getElementById("email").value;
+    let status = document.getElementById("status").value;
+
+    let table = document.getElementById("leadTable");
+
+    let row = table.insertRow();
+
+    row.insertCell(0).innerHTML = name;
+    row.insertCell(1).innerHTML = email;
+    row.insertCell(2).innerHTML = status;
+
+    document.getElementById("name").value = "";
+    document.getElementById("email").value = "";
+}
